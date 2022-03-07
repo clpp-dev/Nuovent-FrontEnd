6e11846ad37d87b7be643855aa88996825f9122b
clperez029 <88810234+clperez029@users.noreply.github.com>
2022-03-07 18:16:06 -0500

    Add conection with endpoint home for charge anounces in home page

--- src/Components/Layout/Featured/Featured.jsx
+++ src/Components/Layout/Featured/Featured.jsx
@@ -1,22 +1,31 @@
+import React, { useState, useEffect } from "react";
 import "./Style.css";
-import React from "react";
-import PropTypes from "prop-types";
 import { HotelCard } from "../../UI/HotelCard/HotelCard";
 import { TittleH2 } from "../../UI/TittleH2/TittleH2";
 
 export const Featured = () => {
+
+const [lastAnounoces, setLastAnounoces] = useState([])
+
+    const urlApi = "https://nuoventr.herokuapp.com/home"
+    console.log("🚀 ~ line 11 urlApi", urlApi)
+    const fetchLastAnounces = async (url) =>{
+        fetch(url)
+        .then(response => response.json())
+        .then(data => setLastAnounoces(data))
+        .catch(error => console.log(error))
+    }
+
+useEffect(() =>{
+  fetchLastAnounces(urlApi)
+},[])
+
   return (
     <div className="cont-featured">
       <TittleH2 textTittle="Destacados" />
       <div className="cont-Cards-featured">
-        <HotelCard />
-        <HotelCard />
-        <HotelCard />
+        <HotelCard anounces={lastAnounoces}/>
       </div>
     </div>
   );
-};
-
-Featured.propTypes = {
-
 };
